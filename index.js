dcc7496440baec20136283aa4816e3fa24d447a7
Blackie01 <damola24795@gmail.com>
2022-07-29 15:57:06 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+// This function removes the transition on each button after click event
+function removeTransition(e){
+    this.classList.remove('playing');
+}
+
+document.querySelectorAll('.key').forEach (a =>{
+    let audio = a.querySelector('audio');
+    const keys = a.querySelector('button')
+    a.addEventListener('click', event => {
+        audio.play();
+        audio.currentTime = 0;
+        keys.classList.add('playing')
+        keys.addEventListener('transitionend', removeTransition)
+    }); 
+})
+
+
+
+// I found the answer here: https://codereview.stackexchange.com/questions/165300/play-audio-files-when-buttons-are-clicked-in-simon-says
